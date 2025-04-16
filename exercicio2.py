0481bff47a64b037363d8561ce5fd6e22c304faf
Emily Gabrielle Oliveira <53714206817@>
2025-04-16 16:24:36 -0300

    Tentativa de atualização

--- exercicio2.py
+++ exercicio2.py
@@ -1,12 +1,13 @@
 print("Esse programa vai somar os do 1 até o número escolhido!!")
 numero = int(input("Insira um número inteiro positivo: "))
-while numero <= 0:
-    numero = int(input("Invalido. Insira um número que seja positivo: "))
+
 soma = 0
 contador = 1
-
-while contador <= numero:
-    soma += contador
-    contador += 1
+if numero <= 0:
+    print("INVALIDO\n")
+elif numero >=1:
+    while contador <= numero:
+        soma += contador
+        contador += 1
 
 print(f"A soma de 1 até {numero} é: {soma}")
