fb9e7f4bd4eabc2d763c62cd499ad8bb539c9e0c
Emily Gabrielle Oliveira <53714206817@>
2025-04-16 16:35:44 -0300

    Tentativa de atualização

--- exercicio4.py
+++ exercicio4.py
@@ -1,12 +1,10 @@
 print("Esse programa vai mostrar o fatorial do número escolhido!!")
 numero = int(input("Insira um número inteiro para calcular o fatorial: "))
 
-while numero <= 0:
-    numero = int(input("Invalido. Insira um número que seja positivo: "))
-
-fatorial = 1
-
-for i in range(1, numero + 1):
-    fatorial *= i
-
-print(f"O fatorial de {numero} é: {fatorial}")
+if numero <= 0:
+    print("INVALIDO")
+elif numero >= 1:
+    fatorial = 1
+    for i in range(1, numero + 1):
+        fatorial *= i
+    print(f"O fatorial de {numero} é: {fatorial}")
